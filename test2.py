a4aabc5c7d108f36c843404c5e6bf58a852a39f9
mrkeem204 <mrkeem204@gmail.com>
2020-03-06 00:10:17 +0000

    added input command

---
+++ test2.py
@@ -0,0 +1,8 @@
+#!/bin/python
+#This program will print out Hello world.
+name = input ("Enter Your Name: ")
+address = input ("Enter Your Addrees: ")
+postcode = input ("Enter Your Oostcode ")
+print(name, "\n")
+print(address, "\n")
+print(postcode, "\n")
